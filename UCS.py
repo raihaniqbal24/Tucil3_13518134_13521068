37a92ed480173ffef948db6bd7e3b31e897d9b92
Muhammad Raihan Iqbal <raihanvega24@gmail.com>
2023-04-12 19:15:42 +0700

    remove unnecessary comments

--- UCS.py
+++ UCS.py
@@ -25,8 +25,6 @@ def ucs(g, start, goal):
     while heap:
         # Mengambil node dengan cost terkecil dari heap
         (cost, current) = heapq.heappop(heap)
-        # print(current)
-        # print(cost)
 
         # Jika node saat ini merupakan tujuan, maka pencarian selesai
         if current == goal:
@@ -34,14 +32,11 @@ def ucs(g, start, goal):
 
         # Menandai node saat ini sebagai sudah dieksplorasi
         explored.add(current)
-        # print(g.getmoves(current))
 
         # Mengeksplorasi tetangga dari node saat ini
         for move in g.getmoves(current):
-            # print(move)
             next_node = move
             move_cost = g.getBobot()
-            # print(move_cost[current][move])
             cost_to_next_node = move_cost[current][move]
 
             # Menghitung total cost untuk mencapai next_node melalui current
@@ -66,50 +61,3 @@ def ucs(g, start, goal):
     path.reverse()
 
     return path
-
-
-
-
-
-
-
-
-
-# def ucs(g, start, goal):
-#     # Inisiasi open list dan closed list
-#     open_list = queue.PriorityQueue()
-#     open_list.put((0, start)) # mengatur prioritas berdasarkan cost
-#     closed_list = set()
-#     # Inisiasi g(n) untuk start node
-#     g = {start: 0}
-#     # Inisiasi parent node
-#     parent = {}
-
-#     # Loop hingga open list kosong
-#     while not open_list.empty():
-#         # Mengambil node dengan cost terendah
-#         cost, current = open_list.get()
-#         # Menambahkan current node ke closed list
-#         closed_list.add(current)
-
-#         # Jika current node sama dengan goal node
-#         if current == goal:
-#             # Mencari path dari start node ke goal node
-#             path = g.reconstruct_path(parent, current)
-#             return path, g[current]
-
-#         # Loop untuk setiap moves dari current node
-#         for move in g.getMoves(current):
-#             # Menghitung cost baru
-#             new_cost = g[current] +  jarak.euclideanDistance(g.getNodeKoordinat(current), g.getNodeKoordinat(move))
-
-#             # Jika move belum pernah dikunjungi atau cost baru lebih kecil dari cost sebelumnya
-#             if move not in closed_list or new_cost < g[move]:
-#                 # Update g(n) dan parent
-#                 g[move] = new_cost
-#                 parent[move] = current
-#                 # Masukkan node ke open list dengan prioritas berdasarkan cost
-#                 open_list.put((g[move], move))
-
-#     # Jika goal node tidak ditemukan, return None
-#     return None, None
